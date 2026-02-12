ea3fc827145515f3a0a8edeb60edc2bc0ff30abd
manasnazim <f16liontigerhunters@gmail.com>
2026-02-12 19:55:48 +0500

    Header completed

--- portfolio/src/components/Header.jsx
+++ portfolio/src/components/Header.jsx
@@ -8,12 +8,14 @@ function Header() {
         <header className="header">
             <h1>Mohammad Anas Nazim</h1>
             <div className="header-buttons">
-                <a href="#projects" className="btn-primary">View Projects</a>
-                <a href="#contact" className="btn-secondary">Contact Me</a>
+                <a href="#about" className="btn-about">About</a>
+                <a href="#projects" className="btn-primary">Projects</a>
+                <a href="#contact" className="btn-secondary">Contact</a>
             </div>
             <a href={Resume} download className="btn-resume">Resume</a>
             
-            <p>Frontend Web Developer | React.js | Firebase</p>
+            <h4>Frontend Web Developer</h4>
+            <p>Crafting fast, accessible web interfaces <br /> React | Firestore</p>
         </header>
         
     </>
